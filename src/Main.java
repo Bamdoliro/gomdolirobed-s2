5c9ecbcc890e90970d3905b555cfcba4142290cd
huise0ng <shinhuiseong07@gmail.com>
2024-06-23 11:59:50 +0900

    ADD :: 간단한 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,67 @@
+package com.bamdoliro.gomdolirobed;
+
 public class Main {
     public static void main(String[] args) {
 
         // TODO-1 도서관 생성
-        // 부산소프트웨어마이스터고등학교 도서관(이하 도서관)을 생성합니다. 도서관에는 다음과 같은 책들이 있습니다.
-        // 자바의 정석 저자 남궁성 ISBN 9788994492032
-        // 객체지향의 사실과 오해 저자 조영호 ISBN 9788998139766
-        // 모던 자바 인 액션 저자 라울 게이브리얼 우르마 ISBN 9791162242025
-
+        Library library = new Library();
+        library.addBook(new Book("자바의 정석", "남궁성", "9788994492032"));
+        library.addBook(new Book("객체지향의 사실과 오해", "조영호", "9788998139766"));
+        library.addBook(new Book("모던 자바 인 액션", "라울 게이브리얼 우르마", "9791162242025"));
 
         // TODO-2 이용자 생성
-        // 도서관 이용자 이밤돌을 생성합니다.
-
-        // 도서관 이용자 김이로를 생성합니다.
-
+        User leeBamdol = new User("이밤돌");
+        User kimIro = new User("김이로");
 
         // TODO-3 도서 추가
-        // 도서관에 모던 자바 인 액션 저자 라울-게이브리얼-우르마 ISBN 9791162242025 이 한권 더 들어옵니다.
-
+        library.addBook(new Book("모던 자바 인 액션", "라울 게이브리얼 우르마", "9791162242025"));
 
         // TODO-4 도서 대출
-        // 이밤돌이 도서관에서 자바의 정석을 대출합니다.
-
-        // 이밤돌이 도서관에서 모던 자바 인 액션을 대출합니다.
-
-        // 김이로가 도서관에서 모던 자바 인 액션을 대출합니다.
-
+        leeBamdol.borrowBook(library, "9788994492032");
+        leeBamdol.borrowBook(library, "9791162242025");
+        kimIro.borrowBook(library, "9791162242025");
 
         // TODO-5 이밤돌 도서 초과 대출 시도
-        // 이밤돌이 도서관에서 객체지향의 사실과 오해를 대출합니다.
-
-        // 대출은 정상적으로 처리되지 않고, 최대 2권이 책까지만 대출할 수 있다는 메시지를 출력합니다.
-
+        if (!leeBamdol.borrowBook(library, "9788998139766")) {
+            System.out.println("최대 2권의 책까지만 대출할 수 있습니다.");
+        }
 
         // TODO-6 이밤돌 도서 반납
-        // 이밤돌이 도서관에 자바의 정석을 반납합니다.
-
+        leeBamdol.returnBook(library, "9788994492032");
 
         // TODO-7 이밤돌 도서 대출
-        // 이밤돌이 도서관에서 객체지향의 사실과 오해를 대출합니다.
-
+        leeBamdol.borrowBook(library, "9788998139766");
 
         // TODO-8 김이로 이미 대출된 도서 대출 시도
-        // 김이로가 도서관에서 객체지향의 사실과 오해를 대출합니다.
-
-        // 이밤돌이 이미 대출했으므로 대출은 정상적으로 처리되지 않고, 다른 사람이 대출한 책은 대출할 수 없다는 메시지를 출력합니다.
-
+        if (!kimIro.borrowBook(library, "9788998139766")) {
+            System.out.println("다른 사람이 대출한 책은 대출할 수 없습니다.");
+        }
 
         // TODO-9 이밤돌 도서 반납
-        // 이밤돌이 도서관에 객체지향의 사실과 오해를 반납합니다.
-
+        leeBamdol.returnBook(library, "9788998139766");
 
         // TODO-10 김이로 도서 대출
-        // 김이로가 도서관에서 객체지향의 사실과 오해를 대출합니다.
-
+        kimIro.borrowBook(library, "9788998139766");
 
         // TODO-11 김이로 대출하지 않은 책 반납 시도
-        // 김이로가 도서관에 자바의 정석 반납합니다.
-
-        // 반납은 정상적으로 처리되지 않고, 대출하지 않은 책은 반납할 수 없다는 메시지를 출력합니다.
-
+        if (!kimIro.returnBook(library, "9788994492032")) {
+            System.out.println("대출하지 않은 책은 반납할 수 없습니다.");
+        }
 
         // TODO-12 전체 조회
-        // 도서관에서 도서 목록을 조회합니다.
+        System.out.println("도서 목록:");
+        for (Book book : library.getAvailableBooks()) {
+            System.out.println(book.getTitle() + " - " + book.getAuthor());
+        }
 
-        // 이밤돌이 본인이 대출한 도서를 조회합니다.
+        System.out.println("이밤돌이 대출한 도서 목록:");
+        for (Book book : leeBamdol.getBorrowedBooks()) {
+            System.out.println(book.getTitle() + " - " + book.getAuthor());
+        }
 
-        // 김이로가 본인이 대출한 도서를 조회합니다.
+        System.out.println("김이로가 대출한 도서 목록:");
+        for (Book book : kimIro.getBorrowedBooks()) {
+            System.out.println(book.getTitle() + " - " + book.getAuthor());
+        }
     }
 }
